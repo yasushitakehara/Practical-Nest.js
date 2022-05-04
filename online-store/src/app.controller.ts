287c1478a42497d0270cba8ac0c5395b1b20c0f2
Yasushi Takehara <yasushitakehara@Yasushis-MacBook-Air-2020.local>
2022-05-04 21:40:23 +0900

    after npm run lint

--- online-store/src/app.controller.ts
+++ online-store/src/app.controller.ts
@@ -1,35 +1,35 @@
-import { Controller, Get, Render } from '@nestjs/common';
-//import { AppService } from './app.service';
+import {Controller, Get, Render} from '@nestjs/common';
+// import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  //constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
 
   //  @Get()
   //  getHello(): string {
   //    return '<b>Hello World2!</b>';
   //  }
-  @Get("/")
+  @Get('/')
   @Render('index')
-  index(){
-    let viewData = [];
+  index() {
+    const viewData = [];
     viewData['title'] = 'Home Page - Online Store';
     return {
-      viewData: viewData
-    }
+      viewData: viewData,
+    };
   }
 
-  @Get("/about")
+  @Get('/about')
   @Render('about')
-  about(){
-    let viewData = [];
+  about() {
+    const viewData = [];
     viewData['title'] = 'About us - Online Store';
-    viewData['subtitle'] = "About us";
-    viewData['description'] = "This is an about page ...";
-    viewData['author'] = "Developed by: Yasushi Takehara";
-    let data1 = 'About us - Online Store';
+    viewData['subtitle'] = 'About us';
+    viewData['description'] = 'This is an about page ...';
+    viewData['author'] = 'Developed by: Yasushi Takehara';
+    const data1 = 'About us - Online Store';
     return {
-      viewData: viewData
+      viewData: viewData,
     };
   }
 }
--- online-store/src/main.ts
+++ online-store/src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {NestExpressApplication} from '@nestjs/platform-express';
+import {join} from 'path';
 import * as hbs from 'hbs';
 import * as hbsUtils from 'hbs-utils';
 
 async function bootstrap() {
-  //const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
