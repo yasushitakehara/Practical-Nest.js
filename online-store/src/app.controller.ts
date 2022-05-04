6eb03446fcb361a43c6cb8784b590ded8089658c
Yasushi Takehara <yasushitakehara@Yasushis-MacBook-Air-2020.local>
2022-05-04 21:43:07 +0900

    modified eslint settings

--- online-store/src/app.controller.ts
+++ online-store/src/app.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Render} from '@nestjs/common';
+import { Controller, Get, Render } from "@nestjs/common";
 // import { AppService } from './app.service';
 
 @Controller()
@@ -9,25 +9,24 @@ export class AppController {
   //  getHello(): string {
   //    return '<b>Hello World2!</b>';
   //  }
-  @Get('/')
-  @Render('index')
+  @Get("/")
+  @Render("index")
   index() {
     const viewData = [];
-    viewData['title'] = 'Home Page - Online Store';
+    viewData["title"] = "Home Page - Online Store";
     return {
       viewData: viewData,
     };
   }
 
-  @Get('/about')
-  @Render('about')
+  @Get("/about")
+  @Render("about")
   about() {
     const viewData = [];
-    viewData['title'] = 'About us - Online Store';
-    viewData['subtitle'] = 'About us';
-    viewData['description'] = 'This is an about page ...';
-    viewData['author'] = 'Developed by: Yasushi Takehara';
-    const data1 = 'About us - Online Store';
+    viewData["title"] = "About us - Online Store";
+    viewData["subtitle"] = "About us";
+    viewData["description"] = "This is an about page ...";
+    viewData["author"] = "Developed by: Yasushi Takehara";
     return {
       viewData: viewData,
     };
