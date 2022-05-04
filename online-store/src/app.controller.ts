c1b22a1735d46b068c2df9d5a7168f564a88169e
Yasushi Takehara <yasushitakehara@Yasushis-MacBook-Air-2020.local>
2022-05-04 21:39:22 +0900

    update

--- online-store/src/app.controller.ts
+++ online-store/src/app.controller.ts
@@ -11,5 +11,25 @@ export class AppController {
   //  }
   @Get("/")
   @Render('index')
-  index(){}
+  index(){
+    let viewData = [];
+    viewData['title'] = 'Home Page - Online Store';
+    return {
+      viewData: viewData
+    }
+  }
+
+  @Get("/about")
+  @Render('about')
+  about(){
+    let viewData = [];
+    viewData['title'] = 'About us - Online Store';
+    viewData['subtitle'] = "About us";
+    viewData['description'] = "This is an about page ...";
+    viewData['author'] = "Developed by: Yasushi Takehara";
+    let data1 = 'About us - Online Store';
+    return {
+      viewData: viewData
+    };
+  }
 }
